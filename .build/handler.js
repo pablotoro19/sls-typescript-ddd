03d65325e17078f2540fd3a5ead946298edf2a16
Pablo Toro <pablo.toro@xepelin.com>
2022-09-08 19:02:22 -0300

    Delete .build directory

--- .build/handler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCustomerById = void 0;
-var index_1 = require("./src/customer/infra/index");
-Object.defineProperty(exports, "getCustomerById", { enumerable: true, get: function () { return index_1.getCustomerById; } });
-//# sourceMappingURL=handler.js.map
