f5b9311f55f4acd0740ed16f0410a98643d84b90
Pablo Toro <pablo.toro@xepelin.com>
2022-09-07 22:21:44 -0400

    add dynamodb

--- serverless.ts
+++ serverless.ts
@@ -1,7 +1,6 @@
 import type { AWS } from '@serverless/typescript';
 
 const serverlessConfiguration: AWS = {
-  org: 'pablotoro',
   service: 'sls-motorbike-shop',
   frameworkVersion: '3',
   useDotenv: true,
@@ -15,9 +14,36 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      NODE_ENV: '${env:NODE_ENV}',
+      DYNAMODB_TABLE_NAME: '${env:DYNAMODB_TABLE_NAME}',
+      DYNAMODB_TABLE_ARN: '${env:DYNAMODB_TABLE_ARN}',
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: [
+              'dynamodb:Query',
+              'dynamodb:Scan',
+              'dynamodb:GetItem',
+              'dynamodb:PutItem',
+              'dynamodb:UpdateItem',
+            ],
+            Resource: ['${env:DYNAMODB_TABLE_ARN}/*', '${env:DYNAMODB_TABLE_ARN}'],
+          },
+        ],
+      },
     },
   },
-
+  custom: {
+    tableName: '${env:DYNAMODB_TABLE_NAME}',
+    dynamodb: {
+      stages: ['dev'],
+      start: { port: 8000, inMemory: true, migrate: true },
+      migration: { dir: 'offline/migrations' },
+    },
+  },
   functions: {
     getCustomerById: {
       handler: 'handler.getCustomerById',
@@ -32,7 +58,33 @@ const serverlessConfiguration: AWS = {
       ],
     },
   },
-  plugins: ['serverless-plugin-typescript', 'serverless-offline'],
+  resources: {
+    Resources: {
+      Customers: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          AttributeDefinitions: [
+            {
+              AttributeName: 'customerId',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'customerId',
+              KeyType: 'HASH',
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+          TableName: 'Customers',
+        },
+      },
+    },
+  },
+  plugins: ['serverless-plugin-typescript', 'serverless-offline', 'serverless-dynamodb-local'],
 };
 
 module.exports = serverlessConfiguration;
